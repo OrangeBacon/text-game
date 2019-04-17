618325c12a8170e719977063eb4672f21f0f7341
Will Moreton <computer.backup.15@gmail.com>
2019-04-17 20:57:07 +0100

    only write array on resize

--- script/console.js
+++ script/console.js
@@ -6,31 +6,49 @@ class DOMConsole {
     this.node.appendChild(this.screen);
     this.charWidth = 15;
     this.charHeight = 20;
+    this.onResize = () => { };
     this.calcSize();
   }
 
+  set onResize(value) {
+    this.onResizeFn = value;
+    this.onResizeFnDebounce = debounce(value, 250);
+  }
+
   set textSize(value) {
-    this.charWidth = getTextWidth("m", `${value-1}px monospace`);
+    this.charWidth = getTextWidth("m", `${value - 1}px monospace`);
     this.charHeight = value;
     this.screen.style.fontSize = `${value - 1}px`;
     this.screen.style.lineHeight = `${value}px`;
+    this.calcSize();
   }
 
   calcSize() {
+    if (this.pixelWidth == this.node.clientWidth &&
+      this.pixelHeight == this.node.clientHeight &&
+      this.oldFontSize == this.charHeight) {
+      return;
+    }
+    this.pixelWidth = this.node.clientWidth;
+    this.pixelHeight = this.node.clientHeight;
     this.columns = (this.node.clientWidth - (this.node.clientWidth % this.charWidth)) / this.charWidth;
     this.lines = (this.node.clientHeight - (this.node.clientHeight % this.charHeight)) / this.charHeight;
+    this.node.childNodes[0].style.width = `${this.columns * this.charWidth}px`;
+    this.node.childNodes[0].style.height = `${this.lines * this.charHeight}px`;
+    this.onResizeFnDebounce();
   }
 
   render() {
     this.calcSize();
-    this.node.childNodes[0].style.width = `${this.columns * this.charWidth}px`;
-    this.node.childNodes[0].style.height = `${this.lines * this.charHeight}px`;
   }
 
   writeArray(arr) {
     this.screen.innerText = "";
     arr.forEach(line => {
-      this.screen.append(document.createTextNode(line.join("")+"\n"));
+      const lineEl = document.createElement("span");
+      lineEl.classList.add("line");
+      lineEl.append(document.createTextNode(line.join("") + "\n"));
+      this.screen.append(lineEl);
     });
   }
 }
@@ -41,3 +59,18 @@ function getTextWidth(text, font) {
   ctx.font = font;
   return ctx.measureText(text).width;
 }
+
+function debounce(func, wait, immediate) {
+  var timeout;
+  return function debounceClosure() {
+    var context = this, args = arguments;
+    var later = function later() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
--- script/main.js
+++ script/main.js
@@ -2,7 +2,9 @@ const state = {};
 
 function initialise() {
   state.screen = new DOMConsole(document.getElementsByClassName("console")[0]);
+  state.screen.onResize = resize;
   state.screen.textSize = 14;
+  resize();
 
   requestAnimationFrame(main);
 }
@@ -10,5 +12,8 @@ function initialise() {
 function main() {
   requestAnimationFrame(main);
   state.screen.render();
+}
+
+function resize() {
   state.screen.writeArray(new Array(state.screen.lines).fill(new Array(state.screen.columns).fill('.')));
 }
